fe094084aca5d2223ab39245dc15d432b960f5e3
강호진 <battlemech@naver.com>
2021-09-24 10:06:47 +0900

    Hook - useState

---
+++ cra/src/App.function.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UseStatecomp from './components/UseStateComp';
+
+function App() {
+    return (
+        <>
+            <h2>Function Component</h2>
+            <UseStatecomp
+                contents = "[함수형 컴포넌트]" />
+        </>
+    )
+}
+
+export default App;
